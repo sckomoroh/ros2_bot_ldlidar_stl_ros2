581fd195b37bc1bca0790caca215494982e0cd91
Yehor Zvihunov <yehor.zvihunov@globallogic.com>
2023-10-24 20:06:15 +0300

    Add parameters to launch file

--- launch/ld19.launch.py
+++ launch/ld19.launch.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
 
-'''
+"""
 Parameter Description:
 ---
 - Set laser scan directon: 
@@ -19,41 +21,48 @@ Parameter Description:
       {'angle_crop_min': 135.0}
       {'angle_crop_max': 225.0}
       which is [135.0, 225.0], angle unit is degress.
-'''
+"""
+
 
 def generate_launch_description():
-  # LDROBOT LiDAR publisher node
-  ldlidar_node = Node(
-      package='ldlidar_stl_ros2',
-      executable='ldlidar_stl_ros2_node',
-      name='LD19',
-      output='screen',
-      parameters=[
-        {'product_name': 'LDLiDAR_LD19'},
-        {'topic_name': 'scan'},
-        {'frame_id': 'lidar_link'},
-        {'port_name': '/dev/ttyUSB0'},
-        {'port_baudrate': 230400},
-        {'laser_scan_dir': True},
-        {'enable_angle_crop_func': False},
-        {'angle_crop_min': 135.0},
-        {'angle_crop_max': 225.0}
-      ]
-  )
+    port = LaunchConfiguration("port")
+    port_cmd = DeclareLaunchArgument(
+        name='port',
+        default_value="/dev/ttyUSB0",
+        description='Lidar port.')
 
-  # base_link to base_laser tf node
-  base_link_to_laser_tf_node = Node(
-    package='tf2_ros',
-    executable='static_transform_publisher',
-    name='base_link_to_base_laser_ld19',
-    arguments=['0','0','0.18','0','0','0','base_link','base_laser']
-  )
+    # LDROBOT LiDAR publisher node
+    ldlidar_node = Node(
+        package="ldlidar_stl_ros2",
+        executable="ldlidar_stl_ros2_node",
+        name="LD19",
+        output="screen",
+        parameters=[
+            {"product_name": "LDLiDAR_LD19"},
+            {"topic_name": "scan"},
+            {"frame_id": "lidar_link"},
+            {"port_name": port},
+            {"port_baudrate": 230400},
+            {"laser_scan_dir": True},
+            {"enable_angle_crop_func": False},
+            {"angle_crop_min": 135.0},
+            {"angle_crop_max": 225.0},
+        ],
+    )
 
+    # base_link to base_laser tf node
+    base_link_to_laser_tf_node = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        name="base_link_to_base_laser_ld19",
+        arguments=["0", "0", "0.18", "0", "0", "0", "base_link", "base_laser"],
+    )
 
-  # Define LaunchDescription variable
-  ld = LaunchDescription()
+    # Define LaunchDescription variable
+    ld = LaunchDescription()
 
-  ld.add_action(ldlidar_node)
-  ld.add_action(base_link_to_laser_tf_node)
+    ld.add_action(port_cmd)
+    ld.add_action(ldlidar_node)
+    # ld.add_action(base_link_to_laser_tf_node)
 
-  return ld
+    return ld
